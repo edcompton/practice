42a22f22e6b292a6452d51e62558a4412f2d18c6
edcompton <edmund.compton@red-badger.com>
2020-11-30 16:51:26 +0000

    part solution for day three.

--- rust/advent-of-code/advent_of_code/src/day_three.rs
+++ rust/advent-of-code/advent_of_code/src/day_three.rs
@@ -1,6 +1,7 @@
 // Two wires are connected centrally and extend outward
 // Trace path each wire takes, which is stored as text
 // Aim is to find the point that both wires cross closest to the central port
+// For each of the instruction vectors, carry out the instructions and
 use super::error::Error;
 use std::fs;
 
@@ -8,10 +9,9 @@ const FILENAME1: &str = "./inputs/day_three/input.txt";
 
 pub fn run() -> Result<Vec<i32>, Error> {
     let file_input = get_file_input(FILENAME1);
-    println!("{:?}", file_input);
     let (first_wire, second_wire) = create_instruction_arrays(file_input);
-
-    println!("{:?}", first_wire);
+    let first_coordinates = get_coordinates(first_wire);
+    println!("{:?}", first_coordinates);
     Ok(vec![2345 as i32])
 }
 
@@ -24,23 +24,41 @@ fn get_file_input(filename: &str) -> Vec<String> {
         .collect()
 }
 
-fn create_instruction_arrays(
-    file_input: Vec<String>,
-) -> (Vec<(String, String)>, Vec<(String, String)>) {
-    let result: Vec<Vec<(String, String)>> = file_input
+fn create_instruction_arrays(file_input: Vec<String>) -> (Vec<(String, i32)>, Vec<(String, i32)>) {
+    let result: Vec<Vec<(String, i32)>> = file_input
         .into_iter()
         .map(|v| {
             v.split(",")
                 .map(|s| {
                     let inner_val = s.to_string();
                     let split_val = inner_val.split_at(1);
-                    (split_val.0.to_string(), split_val.1.to_string())
+                    let parsed_val: i32 = split_val.1.parse().unwrap();
+                    (split_val.0.to_string(), parsed_val)
                 })
-                .collect::<Vec<(String, String)>>()
+                .collect::<Vec<(String, i32)>>()
         })
         .collect();
 
     // println!("{:?}", result);
 
     (result[0].clone(), result[1].clone())
+}
+
+fn get_coordinates(input: Vec<(String, i32)>) {
+    let move_coordinates = input.into_iter().fold(vec![(0, 0)], |acc, val| {
+        let move_amount = val.1;
+        let previous_index = acc.len() - 1;
+        let x = acc[previous_index].0;
+        let y = acc[previous_index].1;
+
+        let instruction = val.0;
+        match instruction.as_str() {
+            "U" => acc.push((acc[previous_index].0, y + move_amount)),
+            "D" => acc.push((acc[previous_index].0, y - move_amount)),
+            "R" => acc.push((x + move_amount, acc[previous_index].0)),
+            "L" => acc.push((x - move_amount, acc[previous_index].0)),
+            _ => panic!("Shit"),
+        };
+        acc
+    });
 }
