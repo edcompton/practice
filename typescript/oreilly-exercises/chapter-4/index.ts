f2af9bcdbb6049678d86d50821e3ad46e6ec1de9
edcompton <edmund.compton@red-badger.com>
2021-11-08 08:31:12 +0000

    fix(config): Refactor gitignore scope and add orielly typescript exercises.

---
+++ typescript/oreilly-exercises/chapter-4/index.ts
@@ -0,0 +1,34 @@
+// Challenge - Second arg must always be string
+function call<T extends [unknown, string, ...unknown[]], R>(
+  f: (...args: T) => R, 
+  ...args: T 
+): R { 
+  return f(...args)
+}
+
+function fill(length: number, value: string): string[] {
+  return Array.from({length}, () => value)
+}
+call(fill, 10, 'a') // evaluates to an array of 10 'a's
+
+
+// Create a typesafe library
+function is<T>(firstArg: T, ...secondArg: [T, ...T[]]): boolean {
+  return secondArg.every(_ => _ === firstArg);
+}
+
+// Compare a string and a string
+console.log(is('string', 'otherstring')) // false
+
+// Compare a boolean and a boolean
+console.log(is(true, false)) // false
+
+// Compare a number and a number
+console.log(is(42, 42)) // true
+
+// Comparing two different types should give a compile-time error
+console.log(is(10, 'foo')) // Error TS2345: Argument of type '"foo"' is not assignable
+              // to parameter of type 'number'.
+
+// [Hard] I should be able to pass any number of arguments
+is([1], [1, 2], [1, 2, 3]) // false
